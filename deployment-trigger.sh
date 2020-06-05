f0792066b625adef5ed5d3d50b53a3ddecc9dc41
reflectpublic <65680923+reflectpublic@users.noreply.github.com>
2020-06-05 13:53:38 -0400

    Create deployment-trigger.sh
    Add a top-level script to execute when a new deployment completes.

---
+++ deployment-trigger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# Execute commands after Release environment is fully deployed.
+#
+
+Tag="release-tests"
+echo "Executing Reflect tests for tag: $Tag"
+curl -v -X POST \
+  -H "X-API-KEY: ${REFLECT_API_KEY}" \
+  -d "{}" \
+  https://api.reflect.run/v1/tags/${Tag}/executions
