6c02829d0bf7a37aad86f378aa295ea615a30646
reflectpublic <65680923+reflectpublic@users.noreply.github.com>
2020-06-06 12:45:45 -0400

    Update deployment-trigger.sh

--- vote/deployment-trigger.sh
+++ vote/deployment-trigger.sh
@@ -6,11 +6,11 @@
 
 Tag="release-tests"
 
-echo "Triggering Reflect tests:"
+echo "=== Triggering Reflect tests ==="
 echo "Tag=${Tag}"
 echo "Host=${VOTE_INGRESS_HOST}"
 
-#curl -v -X POST \
-#  -H "X-API-KEY: ${REFLECT_API_KEY}" \
-#  -d "{}" \
-#  https://api.reflect.run/v1/tags/${Tag}/executions
+curl -v -X POST \
+  -H "X-API-KEY: ${REFLECT_API_KEY}" \
+  -d "{}" \
+  https://api.reflect.run/v1/tags/${Tag}/executions
